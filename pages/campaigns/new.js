e931b9b621b3bafebea2e6f0ed6fbd99f5cf0fce
bernardo mendonça <bernardomendonca@outlook.com>
2021-10-16 13:07:30 +1100

    used semantic UI to create form for new campaign

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
-import Layout from '../../components/Layout'
+import { Form, Button, Input } from 'semantic-ui-react';
+import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3'
 
 class CampaignNew extends Component {
+    state = {
+        minimumContribution: ''
+    }
+
+    onSubmit = async (event) => {
+        // preventing the browsere to submit the form to our backend
+        event.preventDefault();
+    
+        // using web3 to get a list of accounts through metamask
+        const accounts = await web3.eth.getAccounts();
+
+        await factory.methods
+            .createCampaign(this.state.minimumContribution)
+            .send({
+                from: accounts[0]
+            });
+    }
+
     render() {
         return (
             <Layout>
-                <h1>new campaign!</h1>
+                <h3>Createa a new campaign</h3>
+
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Field>
+                        <label>Minimum Contribution</label>
+                        <Input 
+                        label="wei" 
+                        labelPosition="right" 
+                        value={this.state.minimumContribution}
+                        onChange={event => this.setState({ minimumContribution: event.target.value })}
+                        />
+                    </Form.Field>
+                    <Button primary>
+                        Create new campaign
+                    </Button>
+                </Form>
             </Layout>
 
         );
